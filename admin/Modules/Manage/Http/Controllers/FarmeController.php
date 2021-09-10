f20fcb8fcbf8c735fc79e6b30b01be560e2cc727
rusdee302 <53857245+rusdee302@users.noreply.github.com>
2021-09-10 09:39:54 +0700

    repo กับ contro

--- admin/Modules/Manage/Http/Controllers/FarmeController.php
+++ admin/Modules/Manage/Http/Controllers/FarmeController.php
@@ -6,14 +6,16 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Manage\Repositories\Repository as Repository;
+use Modules\Manage\Repositories\FarmesRepository as FarmesRepository;
 
 class FarmeController extends Controller
 {
     protected $Repository;
 
-    public function __construct(Repository $Repository)
+    public function __construct(Repository $Repository,FarmesRepository $FarmesRepository)
     {
         $this->Repository = $Repository;
+        $this->FarmesRepository = $FarmesRepository;
     }
 
     /**
@@ -25,7 +27,12 @@ class FarmeController extends Controller
         $page_title = 'กลุ่มเกษตรกร และฟาร์ม';
         $page_description = '';
 
-        return view('manage::farme.manage_farme', compact('page_title', 'page_description'));
+        $db = "farmes";
+        $data['result'] = $this->FarmesRepository->index($db);
+        dd($data);
+
+        // dd($data);
+        return view('manage::farme.manage_farme', compact('page_title', 'page_description'),$data);
     }
 
     /**
@@ -51,15 +58,71 @@ class FarmeController extends Controller
      */
     public function CreateFarme(Request $request)
     {
-        $page_title = 'เพิ่มข้อมูลกลุ่มเกษตรกร และฟาร์ม';
-        $page_description = '';
+        // $page_title = 'เพิ่มข้อมูลกลุ่มเกษตรกร และฟาร์ม';
+        // $page_description = '';
 
-        // dd($request);
+        $datajount['resultID'] = $this->Repository->ProvinceJoin($request['FA_SUB_DISTRICT']);
+        // dd($datajount['resultID']['result'][0]->id_provinces);
+        $request['FA_DISTRICT'] = $datajount['resultID']['result'][0]->id_amphures;
+        $request['FA_PROVINCE'] = $datajount['resultID']['result'][0]->id_provinces;
+
+        $request['FA_FLOWER'] = serialize($request['FA_FLOWER']);
+        $request['FA_CUSTOMER_GROUP'] = serialize($request['FA_CUSTOMER_GROUP']);
+        $request['FA_SEND_OTHER'] = serialize($request['FA_SEND_OTHER']);
+        $request['FA_CONDITION_SELL_OTHER'] = serialize($request['FA_CONDITION_SELL_OTHER']);
+        $request['FA_CUSTOMER_PAYFA_OTHER'] = serialize($request['FA_CUSTOMER_PAYFA_OTHER']);
+        $request['FA_PROMOTION_OTHER'] = serialize($request['FA_PROMOTION_OTHER']);
+
+        $request['file'] = "111";
+        $request['file_multiple'] = "222";
+        
+        $datas = $request->all();
+        // 
+        $data['resulta'] = $this->Repository->insert($datas,'classModelFarmes');
+        // dd($data['result']['id']);
+        $data['resultID'] = $this->FarmesRepository->ShowId($data['resulta']['id'],'farmes');
+
+        // dd($data['resulta']['id']);
         $data['result'] = $this->Repository->show('flowers');
         $data['resultAmphures'] = $this->Repository->show('amphures');
         $data['resultProvinces'] = $this->Repository->show('provinces');
         $data['resultDistricts'] = $this->Repository->districts('provinces');
 
-        return view('manage::farme.form_farme_part2', compact('page_title', 'page_description'));
+
+        // return view('manage::farme.form_farme_part2', compact('page_title', 'page_description'),$data);
+        return redirect()->route('manage.create.farme2',$data['resulta']['id']);
+    }
+
+    public function FormFarme2($id)
+    {
+        $page_title = 'เพิ่มข้อมูลกลุ่มเกษตรกร และฟาร์ม';
+        $page_description = '';
+        
+        $data['resultID'] = $this->FarmesRepository->ShowId($id,'farmes');
+
+        $data['result'] = $this->Repository->show('flowers');
+        $data['resultAmphures'] = $this->Repository->show('amphures');
+        $data['resultProvinces'] = $this->Repository->show('provinces');
+        $data['resultDistricts'] = $this->Repository->districts('provinces');
+
+        return view('manage::farme.form_farme_part2', compact('page_title', 'page_description'),$data);
+    }
+
+    public function CreateFarme2(Request $request,$id)
+    {
+        $page_title = 'เพิ่มข้อมูลร้านค้า';
+        $page_description = '';
+
+        
+        $request['FA_VOLUME'] = serialize($request['FA_VOLUME']);
+        $request['FA_REMAINING'] = serialize($request['FA_REMAINING']);
+        $request['FA_REMAINING_CAUSE_OTHER'] = serialize($request['FA_REMAINING_CAUSE_OTHER']);
+        $request['FA_SET_PRICE'] = serialize($request['FA_SET_PRICE']);
+        $request['FA_PROBLEM'] = serialize($request['FA_PROBLEM']);
+        // dd($request,$id);
+        $datas = $request->all();
+        $data['result'] = $this->Repository->update($datas,$id,'farmes');
+        
+        return redirect()->route('index.farme');
     }
 }
