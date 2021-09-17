3c2901418e681765418a68e21a884b37d0d57075
rusdee302 <53857245+rusdee302@users.noreply.github.com>
2021-09-17 09:50:30 +0700

    file เติม s

--- admin/Modules/Manage/Resources/views/flower/form_flower.blade.php
+++ admin/Modules/Manage/Resources/views/flower/form_flower.blade.php
@@ -86,13 +86,13 @@
                             <div class="col-lg-12" style="margin-top: 20px">
                                 <div class="field" align="left">
                                     <label class="button hollow">อัปโหลดรูปหน้าปก +</label>
-                                    <input type="file" id="file" name="file" />
+                                    <input type="file" id="file" name="files" />
                                 </div>
                             </div>
                             <div class="col-lg-12" style="margin-top: 20px">
                                 <div class="field" align="left">
                                     <label class="button hollow">เพิ่มรูปภาพดอกไม้ทั้งหมด +</label>
-                                    <input type="file" id="files" name="file_multiple[]" multiple />
+                                    <input type="file" id="files" name="file_multiples[]" multiple />
                                 </div>
                             </div>
                         </div>
