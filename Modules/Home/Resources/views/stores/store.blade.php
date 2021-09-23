c68dccfbb7491f63ca820892ba446d46f8f2d174
rusdee302 <53857245+rusdee302@users.noreply.github.com>
2021-09-23 15:44:15 +0700

    ร้าน

--- Modules/Home/Resources/views/stores/store.blade.php
+++ Modules/Home/Resources/views/stores/store.blade.php
@@ -1,16 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
+<style>
+    /* Styles for wrapping the search box */
 
+.main {
+    width: 50%;
+    /* margin: 50px auto; */
+}
+
+/* Bootstrap 3 text input with search icon */
+
+.has-search .form-control-feedback {
+    right: initial;
+    left: 0;
+    color: #ccc;
+}
+
+.has-search .form-control {
+    padding-right: 12px;
+    padding-left: 34px;
+}
+
+</style>
+{{-- @dd($result) --}}
 <section style="background-color: #faeaea">
-    <!-- plant -->
+
     <div id="plant" class="plants" style="margin-top: 170px">
         <div class="container">
         <div class="row">
             <div class="col-md-12 ">
                 <div class="titlepage">
                     <h2>ข้อมูลร้านค้า</h2>
-                    <span>looking at its layout. The point of using Lorem Ipsumletters, as opposed to usingl</span>
+                    {{-- <span>looking at its layout. The point of using Lorem Ipsumletters, as opposed to usingl</span> --}}
+                        <!-- plant -->
+                        <center>
+                            <div class="main">
+                                <!-- Actual search box -->
+                                <div class="form-group has-feedback has-search">
+                                <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                                <input type="text" class="form-control" placeholder="ค้นหา">
+                                </div>
+                            </div>
+                        </center>
                 </div>
             </div>
         </div>
@@ -23,13 +55,19 @@
         </style>
         <div class="container">
             <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12" onclick="window.location='{{ route('home.stores.detail',1) }}'">
+                @foreach ($result as $key => $value)
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12" onclick="window.location='{{ route('home.stores.detail',$value->id) }}'">
                     <div class="plants-box">
-                        <figure><img src="https://ออกแบบร้าน.com/wp-content/uploads/2019/12/024-5-1024x683.jpg" alt="img" class="img_store"/></figure>
-                        <h3> ชื่อร้านค้า</h3>
-                        <p><b>ลักษณะ :</b> It is a long established fact that a reader will be distracted by ...</p>
+                        <figure><img src="{{ $value->file }}" alt="img" class="img_store"/></figure>
+                        {{-- <p style="font-size: 20px" style="margin-top: 5px"><b>{{ $value->S_NAME }}</b></p> --}}
+                        <p style="font-size: 17px" style="margin-top: 10px">
+                            <b>{{ $value->S_NAME }}</b><br>
+                            <b>ที่อยู่ :</b> ต.{{ $ProvinceJoin[$key][0]->name_districts }} อ.{{ $ProvinceJoin[$key][0]->name_amphures }} จ.{{ $ProvinceJoin[$key][0]->name_provinces }}
+                            
+                        </p>
                     </div>
-                </div>
+                </div>    
+                @endforeach
             </div>
         </div>
     </div>
--- Modules/Home/Resources/views/stores/store_detail.blade.php
+++ Modules/Home/Resources/views/stores/store_detail.blade.php
@@ -1,4 +1,4 @@
-@extends('home::layouts.master')
+@extends('layouts.master')
 
 @section('content')
 
@@ -9,53 +9,58 @@
           <div class = "product-imgs">
             <div class = "img-display">
               <div class = "img-showcase">
-                <img src = "https://ออกแบบร้าน.com/wp-content/uploads/2019/12/024-5-1024x683.jpg" alt = "shoe image">
-                <img src = "https://www.loveyouflower.com/wp-content/uploads/2019/08/%E0%B8%94%E0%B8%AD%E0%B8%81%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%9E.jpg" alt = "shoe image">
-                <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQE1I3dhU8Yt6f0L6rZeH7TfqaQ3ADT4i92Q&usqp=CAU" alt = "shoe image">
-                <img src = "https://24flowerphuket.yellowpages.co.th/sites/storage/files/users/b/9/a/9/b9a900f1-022e-454c-9116-d5559629b281/%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%94%E0%B8%AD%E0%B8%81%E0%B9%84%E0%B8%A1%E0%B9%89%20%E0%B8%A0%E0%B8%B9%E0%B9%80%E0%B8%81%E0%B9%87%E0%B8%95%20-%20%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%96%E0%B8%B9%E0%B8%81_0.jpg" alt = "shoe image">
+                <img src = "http://127.0.0.1:8000/storage/flowers/pONkC_LQ6Iq.jpg" alt = "shoe image">
+                <img src = "http://127.0.0.1:8000/storage/flowers/8tQwn_gtqEL.jpg" alt = "shoe image">
+                <img src = "http://127.0.0.1:8000/storage/flowers/HClvZ_ie6BT.jpg" alt = "shoe image">
+                <img src = "http://127.0.0.1:8000/storage/flowers/vGjLO_Ls7g8.jpg" alt = "shoe image">
               </div>
             </div>
             <div class = "img-select">
               <div class = "img-item">
                 <a href = "#" data-id = "1">
-                  <img src = "https://ออกแบบร้าน.com/wp-content/uploads/2019/12/024-5-1024x683.jpg" alt = "shoe image">
+                  <img src = "http://127.0.0.1:8000/storage/flowers/pONkC_LQ6Iq.jpg" alt = "shoe image">
                 </a>
               </div>
               <div class = "img-item">
                 <a href = "#" data-id = "2">
-                  <img src = "https://www.loveyouflower.com/wp-content/uploads/2019/08/%E0%B8%94%E0%B8%AD%E0%B8%81%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%9E.jpg" alt = "shoe image">
+                  <img src = "http://127.0.0.1:8000/storage/flowers/8tQwn_gtqEL.jpg" alt = "shoe image">
                 </a>
               </div>
               <div class = "img-item">
                 <a href = "#" data-id = "3">
-                  <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQE1I3dhU8Yt6f0L6rZeH7TfqaQ3ADT4i92Q&usqp=CAU" alt = "shoe image">
+                  <img src = "http://127.0.0.1:8000/storage/flowers/HClvZ_ie6BT.jpg" alt = "shoe image">
                 </a>
               </div>
               <div class = "img-item">
                 <a href = "#" data-id = "4">
-                  <img src = "https://24flowerphuket.yellowpages.co.th/sites/storage/files/users/b/9/a/9/b9a900f1-022e-454c-9116-d5559629b281/%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%94%E0%B8%AD%E0%B8%81%E0%B9%84%E0%B8%A1%E0%B9%89%20%E0%B8%A0%E0%B8%B9%E0%B9%80%E0%B8%81%E0%B9%87%E0%B8%95%20-%20%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%96%E0%B8%B9%E0%B8%81_0.jpg" alt = "shoe image">
+                  <img src = "http://127.0.0.1:8000/storage/flowers/vGjLO_Ls7g8.jpg" alt = "shoe image">
                 </a>
               </div>
             </div>
           </div>
           <!-- card right -->
           <div class = "product-content">
-            <h2 class = "product-title">ชื่อร้านค้า</h2>
+            <h2 class = "product-title">ณัฐนันท์ดอกไม้สด</h2>
                 <div class = "product-detail">
                     <h3>ที่อยู่ร้าน: ที่อยู่: ตลาดสด เทศบาล เมือง ยะลา อ เมือง ยะลา 95000 </h3>
                     
-                    <h3>ข้อมูลร้าน: </h3>
-                <p>ร้านดอกไม้ทันสมัยอันดับ1 รับจัดช่อดอกไม้ กล่องดอกไม้ เเจกันดอกไม้ กระเช้าดอกไม้ พวงหรีด คัดสรรดอกสวยพรีเมี่ยม การันตีคุณภาพ มีชื่อเสียงด้วยประสบการณ์กว่า 10 ปี บริการสุดประทับใจ ประสบการณ์กว่า 10 ปี มีให้เลือกกว่า 1,000 เเบบ</p>
+                <h3>ข้อมูลร้าน: </h3>
+                <p>รับจัดดอกไม้สด ,แห้ง ในงานพิธีต่างๆทั้งในและนอกสถานที่ 0889595596, ,0809785356, 073-221-897 เทศบาลนครยะลา</p>
+
+                <h3>ข้อมูลติดต่อ : </h3>
+                <p>
+                    <b>เบอร์โทรศัพท์ : </b>  0889595596 <br>
+                    <b>facebook : </b>  ณัฐนันท์ดอกไม้สด <br>
+                    <b>line : </b>  @lapatrada3911 <br>
+                    <b>website : </b>  - <br>
+                </p>
 
                 <h3>ดอกไม้ในร้าน: </h3>
-                <p>ดอกราชพฤกษ์ (คูน) *ดอกไม้ประชำชาติไทย
-                    ดอกปีบ (กาสะลอง) *ดอกไม้ประจำวิชาชีพ พยาบาล
-                    ดอกขจร(สลิด) *เป็นไม้มงคลเชื่อว่าบ้านใดปลูกไว้ บ้านนั้นจะมีชื่อเสียงขจรขจายดังชื่อ
-                    ดอกกล้วยไม้ ช้างกระ *ดอกกล้วยไม้ เป็นดอกไม้ประจำวิชาชีพ ครู
-                    ดอกอัญชัญ ...
-                    ดอกแคแสด
-                    ดอกยี่หุบ
-                    ดอกหางนกยูง</p>
+                <p>
+                    - ดาวเรือง <br>
+                    - เบญจมาศ <br>
+                    - แกลดิโอลัส <br>
+                </p>
                 </div>
         
             </div>
@@ -71,75 +76,51 @@
             <div id="gallery" class="Gallery">
             {{-- <div class="container-fluid margin-r-l"> --}}
                 <div class="row">
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 thumb">
+                    <div class="col-xl-4 col-lg-3 col-md-3 col-sm-12 thumb">
                         <div class="Gallery-box">
                             <figure>
-                                <a href="{{ asset('fonend/images/1.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/1.jpg') }}" class="zoom img-fluid "  alt="">
+                                <a href="http://127.0.0.1:8000/storage/flowers/pONkC_LQ6Iq.jpg" class="fancybox" rel="ligthbox">
+                                <img  src="http://127.0.0.1:8000/storage/flowers/pONkC_LQ6Iq.jpg" class="zoom img-fluid "  alt="">
                                 </a>
                                 <span class="hoverle">
-                                <a href="{{ asset('fonend/images/1.jpg') }}" class="fancybox" rel="ligthbox">View</a>
+                                <a href="http://127.0.0.1:8000/storage/flowers/pONkC_LQ6Iq.jpg" class="fancybox" rel="ligthbox">View</a>
                                 </span>  
                             </figure>
                         </div>
                     </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 thumb">
+                    <div class="col-xl-4 col-lg-3 col-md-3 col-sm-12 thumb">
                         <div class="Gallery-box">
                             <figure>
-                                <a href="{{ asset('fonend/images/2.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/2.jpg') }}" class="zoom img-fluid "  alt="">
+                                <a href="http://127.0.0.1:8000/storage/flowers/8tQwn_gtqEL.jpg" class="fancybox" rel="ligthbox">
+                                <img  src="http://127.0.0.1:8000/storage/flowers/8tQwn_gtqEL.jpg" class="zoom img-fluid "  alt="">
                                 </a>
                                 <span class="hoverle">
-                                <a href="{{ asset('fonend/images/1.jpg') }}" class="fancybox" rel="ligthbox">View</a>
+                                <a href="http://127.0.0.1:8000/storage/flowers/8tQwn_gtqEL.jpg" class="fancybox" rel="ligthbox">View</a>
                                 </span>
                             </figure>
                         </div>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 thumb">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 thumb">
                         <div class="Gallery-box">
                             <figure>
-                                <a href="{{ asset('fonend/images/3.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/3.jpg') }}" class="zoom img-fluid "  alt="">
+                                <a href="http://127.0.0.1:8000/storage/flowers/HClvZ_ie6BT.jpg" class="fancybox" rel="ligthbox">
+                                <img  src="http://127.0.0.1:8000/storage/flowers/HClvZ_ie6BT.jpg" class="zoom img-fluid "  alt="">
                                 </a>
                                 <span class="hoverle">
-                                <a href="{{ asset('fonend/images/3.jpg') }}" class="fancybox" rel="ligthbox">View</a>
+                                <a href="http://127.0.0.1:8000/storage/flowers/HClvZ_ie6BT.jpg" class="fancybox" rel="ligthbox">View</a>
                                 </span>
                             </figure>
                         </div>
                     </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 thumb">
+                    <div class="col-xl-4 col-lg-3 col-md-3 col-sm-12 thumb">
                         <div class="Gallery-box">
                             <figure>
-                                <a href="{{ asset('fonend/images/4.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/4.jpg') }}" class="zoom img-fluid "  alt="">
+                                <a href="http://127.0.0.1:8000/storage/flowers/vGjLO_Ls7g8.jpg"" class="fancybox" rel="ligthbox">
+                                <img  src="http://127.0.0.1:8000/storage/flowers/vGjLO_Ls7g8.jpg"" class="zoom img-fluid "  alt="">
                                 </a>
                                 <span class="hoverle">
-                                <a href="{{ asset('fonend/images/4.jpg') }}" class="fancybox" rel="ligthbox">View</a>
+                                <a href="http://127.0.0.1:8000/storage/flowers/vGjLO_Ls7g8.jpg"" class="fancybox" rel="ligthbox">View</a>
                                 </span> 
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 thumb">
-                        <div class="Gallery-box">
-                            <figure>
-                                <a href="{{ asset('fonend/images/5.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/5.jpg') }}" class="zoom img-fluid "  alt="">
-                                </a>
-                                <span class="hoverle">
-                                <a href="{{ asset('fonend/images/5.jpg') }}" class="fancybox" rel="ligthbox">View</a>
-                                </span> 
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <div class="Gallery-box">
-                            <figure>
-                                <a href="{{ asset('fonend/images/6.jpg') }}" class="fancybox" rel="ligthbox">
-                                <img  src="{{ asset('fonend/images/6.jpg') }}" class="zoom img-fluid "  alt="">
-                                </a>
-                                <span class="hoverle">
-                                <a href="{{ asset('fonend/images/6.jpg') }}" class="fancybox" rel="ligthbox">View</a>
-                                </span>
                             </figure>
                         </div>
                     </div>
